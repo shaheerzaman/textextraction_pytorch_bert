dc63f8040db9c49ede10b86e8cd5f35bb423c24c
shaheerzaman <shaheer_zaman84@yahoo.com>
2020-08-11 16:08:15 +0530

    first commit

---
+++ src/config.py
@@ -0,0 +1,15 @@
+import tokenizers
+
+MAX_LEN=128
+TRAIN_BATCH_SIZE=32
+VALID_BATCH_SIZE=16
+EPOCHS=10
+BERT_PATH='../input/bert_base_uncased/'
+MODEL_PATH='model.bin'
+TRANING_FILE='../input/train.csv'
+TOKENIZER=tokenizers.BertWordPieceTokenizer(
+    os.path.join(BERT_PATH, 'vocab.txt'), 
+    lowercase=True
+)
+
+
---
+++ src/model.py
@@ -0,0 +1,27 @@
+import config
+import transformers
+import torch.nn as nn
+
+class BERTBaseUncased(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.bert = transformers.BertModel.from_pretrained(config.BERT_PATH)
+        self.bert_drop = nn.Dropout(0.3)
+        self.l0 = nn.Linear(768, 2)
+
+    def forward(self, ids, mask, token_type_ids, sentiment):
+        sequence_output, pooled_out = self.bert(
+            ids, 
+            attention_mask=mask, 
+            token_type_ids=token_type_ids, 
+        )
+        #(batch_size, num_tokens, 768)
+        logits = self.l0(sequence_output)
+        #(batch_size, num_tokens, 2)
+        #(batch_size, num_tokens, 1), (batch_size, num_tokens, 1)
+        start_logits, end_logits = logits.split(1, dim=-1)
+        start_logits = start_logits.squeeze(-1)
+        end_logits = end_logits.squeeze(-1)
+        #(batch_size, num_tokens), (batch_size, num_tokens)
+        return start_logits, end_logits
+
